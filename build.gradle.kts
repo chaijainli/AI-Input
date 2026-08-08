3f8bb1df971525694fef497a5df60d1dfae66a0a
chaijianli <657588747@qq.com>
2026-08-08 13:20:06 +0800

    fix: add pluginManagement and repository declarations

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,21 @@
  * Top-level build file.
  * Open with Android Studio, sync Gradle files.
  */
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 plugins {
     id("com.android.application") version "8.5.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
