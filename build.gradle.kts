61e3d197deba83d001f1b04b1c611e47ebd76726
chaijianli <657588747@qq.com>
2026-08-08 13:59:32 +0800

    fix: remove pluginManagement from build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,23 +2,7 @@
  * Top-level build file.
  * Open with Android Studio, sync Gradle files.
  */
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-
 plugins {
     id("com.android.application") version "8.5.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = uri("https://maven.google.com") }
-    }
 }
